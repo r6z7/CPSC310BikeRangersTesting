9c49db86885d82e688abdf1a1a8e206b9114245f
r6z7 <r6z7@ugrad.cs.ubc.ca>
2014-02-27 18:04:44 -0800

    Started implementation of EntryPoint in onModuleLoad().  First draft of UI and backend for importing dataset from a URL - stopped at method that calls url->JSON parser.

--- BikeRangers/src/com/google/gwt/cs310/bikerangers/client/BikeRangers.java
+++ BikeRangers/src/com/google/gwt/cs310/bikerangers/client/BikeRangers.java
@@ -6,12 +6,16 @@ import com.google.gwt.core.client.GWT;
 import com.google.gwt.event.dom.client.ClickEvent;
 import com.google.gwt.event.dom.client.ClickHandler;
 import com.google.gwt.event.dom.client.KeyCodes;
+import com.google.gwt.event.dom.client.KeyDownEvent;
+import com.google.gwt.event.dom.client.KeyDownHandler;
 import com.google.gwt.event.dom.client.KeyUpEvent;
 import com.google.gwt.event.dom.client.KeyUpHandler;
+import com.google.gwt.user.client.Window;
 import com.google.gwt.user.client.rpc.AsyncCallback;
 import com.google.gwt.user.client.ui.Button;
 import com.google.gwt.user.client.ui.DialogBox;
 import com.google.gwt.user.client.ui.HTML;
+import com.google.gwt.user.client.ui.HorizontalPanel;
 import com.google.gwt.user.client.ui.Label;
 import com.google.gwt.user.client.ui.RootPanel;
 import com.google.gwt.user.client.ui.TextBox;
@@ -21,132 +25,89 @@ import com.google.gwt.user.client.ui.VerticalPanel;
  * Entry point classes define <code>onModuleLoad()</code>.
  */
 public class BikeRangers implements EntryPoint {
-	/**
-	 * The message displayed to the user when the server cannot be reached or
-	 * returns an error.
-	 */
-	private static final String SERVER_ERROR = "An error occurred while "
-			+ "attempting to contact the server. Please check your network "
-			+ "connection and try again.";
-
-	/**
-	 * Create a remote service proxy to talk to the server-side Greeting service.
-	 */
-	private final GreetingServiceAsync greetingService = GWT
-			.create(GreetingService.class);
-
+	
+	private VerticalPanel mainPanel = new VerticalPanel();
+	private HorizontalPanel importPanel = new HorizontalPanel();
+	private TextBox newUrlTextBox = new TextBox();
+	private Button importUrlButton = new Button("Import");
+	private Label importPanelTitle = new Label("Admin Page");
+	
+	
 	/**
 	 * This is the entry point method.
 	 */
 	public void onModuleLoad() {
-		final Button sendButton = new Button("Send");
-		final TextBox nameField = new TextBox();
-		nameField.setText("GWT User");
-		final Label errorLabel = new Label();
+		loadBikeRangers();
+	}
 
-		// We can add style names to widgets
-		sendButton.addStyleName("sendButton");
-
-		// Add the nameField and sendButton to the RootPanel
-		// Use RootPanel.get() to get the entire body element
-		RootPanel.get("nameFieldContainer").add(nameField);
-		RootPanel.get("sendButtonContainer").add(sendButton);
-		RootPanel.get("errorLabelContainer").add(errorLabel);
-
-		// Focus the cursor on the name field when the app loads
-		nameField.setFocus(true);
-		nameField.selectAll();
-
-		// Create the popup dialog box
-		final DialogBox dialogBox = new DialogBox();
-		dialogBox.setText("Remote Procedure Call");
-		dialogBox.setAnimationEnabled(true);
-		final Button closeButton = new Button("Close");
-		// We can set the id of a widget by accessing its Element
-		closeButton.getElement().setId("closeButton");
-		final Label textToServerLabel = new Label();
-		final HTML serverResponseLabel = new HTML();
-		VerticalPanel dialogVPanel = new VerticalPanel();
-		dialogVPanel.addStyleName("dialogVPanel");
-		dialogVPanel.add(new HTML("<b>Sending name to the server:</b>"));
-		dialogVPanel.add(textToServerLabel);
-		dialogVPanel.add(new HTML("<br><b>Server replies:</b>"));
-		dialogVPanel.add(serverResponseLabel);
-		dialogVPanel.setHorizontalAlignment(VerticalPanel.ALIGN_RIGHT);
-		dialogVPanel.add(closeButton);
-		dialogBox.setWidget(dialogVPanel);
-
-		// Add a handler to close the DialogBox
-		closeButton.addClickHandler(new ClickHandler() {
+	private void loadBikeRangers() {
+		// Assemble import-data-from-url panel
+		importPanel.add(importPanelTitle);
+		importPanel.add(newUrlTextBox);
+		importPanel.add(importUrlButton);
+		
+		// Assemble main panel
+		mainPanel.add(importPanel);
+		
+		// Associate the Main panel with the HTML host page.
+		RootPanel.get("importURL").add(mainPanel);
+				
+		// Move cursor focus to the input box.
+		newUrlTextBox.setFocus(true);
+		
+		// Listen for mouse events on the Add button.
+		importUrlButton.addClickHandler(new ClickHandler() {
 			public void onClick(ClickEvent event) {
-				dialogBox.hide();
-				sendButton.setEnabled(true);
-				sendButton.setFocus(true);
+				//TODO backend method
+				importDataset();
 			}
 		});
-
-		// Create a handler for the sendButton and nameField
-		class MyHandler implements ClickHandler, KeyUpHandler {
-			/**
-			 * Fired when the user clicks on the sendButton.
-			 */
-			public void onClick(ClickEvent event) {
-				sendNameToServer();
-			}
-
-			/**
-			 * Fired when the user types in the nameField.
-			 */
-			public void onKeyUp(KeyUpEvent event) {
+				
+		// Listen for keyboard events in the input box.
+		newUrlTextBox.addKeyDownHandler(new KeyDownHandler() {
+			public void onKeyDown(KeyDownEvent event) {
 				if (event.getNativeKeyCode() == KeyCodes.KEY_ENTER) {
-					sendNameToServer();
+					//TODO backend method
+					importDataset();
 				}
 			}
-
-			/**
-			 * Send the name from the nameField to the server and wait for a response.
-			 */
-			private void sendNameToServer() {
-				// First, we validate the input.
-				errorLabel.setText("");
-				String textToServer = nameField.getText();
-				if (!FieldVerifier.isValidName(textToServer)) {
-					errorLabel.setText("Please enter at least four characters");
-					return;
-				}
-
-				// Then, we send the input to the server.
-				sendButton.setEnabled(false);
-				textToServerLabel.setText(textToServer);
-				serverResponseLabel.setText("");
-				greetingService.greetServer(textToServer,
-						new AsyncCallback<String>() {
-							public void onFailure(Throwable caught) {
-								// Show the RPC error message to the user
-								dialogBox
-										.setText("Remote Procedure Call - Failure");
-								serverResponseLabel
-										.addStyleName("serverResponseLabelError");
-								serverResponseLabel.setHTML(SERVER_ERROR);
-								dialogBox.center();
-								closeButton.setFocus(true);
-							}
-
-							public void onSuccess(String result) {
-								dialogBox.setText("Remote Procedure Call");
-								serverResponseLabel
-										.removeStyleName("serverResponseLabelError");
-								serverResponseLabel.setHTML(result);
-								dialogBox.center();
-								closeButton.setFocus(true);
-							}
-						});
-			}
-		}
-
-		// Add a handler to send the name to the server
-		MyHandler handler = new MyHandler();
-		sendButton.addClickHandler(handler);
-		nameField.addKeyUpHandler(handler);
+		});
 	}
+
+	
+	/**
+	 * Import data from entered url.  Executed when the user clicks the importUrlButton
+	 * or presses enter in the newUrlTextBox.
+	 */
+	private void importDataset() {
+		// TODO Auto-generated method stub
+		final String url = newUrlTextBox.getText().trim();
+		newUrlTextBox.setFocus(true);
+		
+		// Url must begin with "https://" or "http://".
+		if (!(url.startsWith("http://") || url.startsWith("https://"))) {
+			Window.alert("'" + url + "' is not a valid URL.");
+			newUrlTextBox.selectAll();
+			return;
+		}
+		newUrlTextBox.setText("");
+		
+		parseDataset(url);
+	}
+
+	
+	/**
+	 * Parses dataset from the given URL into JSON objects, then pushes to the
+	 * Google datastore.
+	 * @param url
+	 */
+	private void parseDataset(String url) {
+		// TODO Auto-generated method stub
+		
+		// Call parser
+		
+		// Push to datastore
+		
+	}
+			
 }
